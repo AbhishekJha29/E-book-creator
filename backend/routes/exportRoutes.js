7d2fdda4507c02cd7fec0e53d437028ce5f81647
AbhishekJha29 <abbishekjha2286@gmail.com>
2025-11-29 15:35:32 +0530

    making of the format like padf as a document is allowed

--- backend/routes/exportRoutes.js
+++ backend/routes/exportRoutes.js
@@ -1,11 +1,9 @@
-import React from 'react'
+const express = require("express");
+const router = express.Router();
+const { exportAsPDF , exportAsDocument} = require("../controller/exportController");
+const {protect} = require("../middlewares/authMiddlewares");
 
-const exportRoutes = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+router.get("/:id/pdf", protect, exportAsPDF);
+router.get("/:id/doc", protect, exportAsDocument);
 
-export default exportRoutes
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const connectDB = require("./config/db");
 const authRoutes = require('./routes/authRoutes');
 const bookRoutes = require('./routes/bookRoutes');
 const aiRoutes = require('./routes/aiRoutes');
+const exportRoutes = require('./routes/exportRoutes');
 
 
 const app = express();
@@ -28,6 +29,7 @@ app.use("/backend/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 app.use("/api/ai", aiRoutes);
+app.use("/api/export", exportRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
